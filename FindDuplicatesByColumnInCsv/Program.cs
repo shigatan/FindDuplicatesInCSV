35e5c861d58f2c6ac19c83f0c3146c84930c9241
shigatan <1409tanya@gmail.com>
2018-02-08 15:03:57 -0800

    remove redundant comment

--- FindDuplicatesByColumnInCsv/Program.cs
+++ FindDuplicatesByColumnInCsv/Program.cs
@@ -20,7 +20,6 @@ namespace FindDuplicatesByColumnIsCsv
             string filePath = args[0];
             string columnName = args[1];
 
-            //the main idea is sort values by specified column and then output only rows with duplicated values
             var duplicatesFinder = new DuplicatesFinderFacade();
             duplicatesFinder.Run(filePath, columnName);
             Console.WriteLine("Press Enter to exit");
