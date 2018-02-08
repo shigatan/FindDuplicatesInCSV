48aaae417943e8266c2f6104b2751847e6abdb53
tatyana.shigaeva <1409tanya@gmail.com>
2018-02-08 13:54:31 -0800

    renaming folders

--- FindDuplicatesByColumnInCsv/Properties/AssemblyInfo.cs
+++ FindDuplicatesByColumnInCsv/Properties/AssemblyInfo.cs
@@ -1,20 +1,36 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("FindDuplicatesByColumnInCsv")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FindDuplicatesByColumnIsCsv")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FindDuplicatesByColumnInCsv")]
+[assembly: AssemblyProduct("FindDuplicatesByColumnIsCsv")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: Guid("3772f5bb-1cac-4308-a0a0-24586328c23c")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7bacd98-59a4-4198-92f3-203bae0c0815")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
